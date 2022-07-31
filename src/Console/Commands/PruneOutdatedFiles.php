47040a713b0d47783256e742fc70d9b0146095d1
Yaro WebDev <yg@think.studio>
2022-07-31 18:32:41 +0300

    Rework prune command code

--- src/Console/Commands/PruneOutdatedFiles.php
+++ src/Console/Commands/PruneOutdatedFiles.php
@@ -4,6 +4,7 @@ namespace LaravelDownloadUtil\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
@@ -17,25 +18,49 @@ class PruneOutdatedFiles extends Command
 
     protected $description = 'Delete outdated files';
 
+    protected Filesystem $storage;
+
+    protected string $filterExtension = '';
+
+    protected int $filterSeconds = 0;
+
+
     public function handle(): int
     {
-        $disk    = $this->argument('disk') ?: config('download-util.storage.default');
-        $storage = Storage::disk($disk);
-        /** @var $file */
-        foreach ($storage->allFiles() as $file) {
-            $isDeleteFile = false;
-            if ($seconds = (int) $this->option('seconds')) {
-                $isDeleteFile = Carbon::createFromTimestamp($storage->lastModified($file))->lessThan(Carbon::now()->subSeconds($seconds));
-            }
-            if ($extension = (string) $this->option('extension')) {
-                $isDeleteFile = Str::endsWith($file, '.' . ltrim($extension, '.'));
-            }
+        $disk                  = $this->argument('disk') ?: config('download-util.storage.default');
+        $this->storage         = Storage::disk($disk);
+        $this->filterExtension = (string) $this->option('extension');
+        $this->filterSeconds   = (int) $this->option('seconds');
 
-            if ($isDeleteFile && $storage->delete($file)) {
+        /** @var $file */
+        foreach ($this->storage->allFiles() as $file) {
+            if ($this->needDeleteFile($file) && $this->storage->delete($file)) {
                 $this->info("Deleted: {$file}");
             }
         }
 
         return 0;
+    }
+
+    protected function needDeleteFile(string $file): bool
+    {
+        if ($this->filterSeconds && !$this->filterSeconds($file)) {
+            return false;
+        }
+        if ($this->filterExtension && !$this->filterExtension($file)) {
+            return false;
+        }
+
+        return $this->filterSeconds || $this->filterExtension;
+    }
+
+    protected function filterSeconds(string $file): bool
+    {
+        return Carbon::createFromTimestamp($this->storage->lastModified($file))->lessThan(Carbon::now()->subSeconds($this->filterSeconds));
+    }
+
+    protected function filterExtension(string $file): bool
+    {
+        return Str::endsWith($file, '.'.ltrim($this->filterExtension, '.'));
     }
 }
